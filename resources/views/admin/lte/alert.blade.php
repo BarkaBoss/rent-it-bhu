5ac6644aac92a2a0d0b49c18afcaca6028fb184b
BarkaBoss <bossmajor.biii@gmail.com>
2025-01-26 22:28:16 +0100

    Fixed Alert pop up to Bootstrap 5

--- resources/views/admin/lte/alert.blade.php
+++ resources/views/admin/lte/alert.blade.php
@@ -1,6 +1,6 @@
 @if(session()->has('alert-message'))
     <div class="alert alert-{{ session()->get('alert-type') }} alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-hidden="true"></button>
 {{--        <h5><i class="icon fa fa-info"></i> Alert!</h5>--}}
         <p>{{ session()->get('alert-message') }}</p>
     </div>
