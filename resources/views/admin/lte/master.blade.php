961b64f928a1604f0d22e480c438fe7e20f2e3cf
BarkaBoss <bossmajor.biii@gmail.com>
2025-01-28 23:17:02 +0100

    Added more routes to master

--- resources/views/admin/lte/master.blade.php
+++ resources/views/admin/lte/master.blade.php
@@ -17,6 +17,7 @@
         content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard"
     />
     <!--end::Primary Meta Tags-->
+    <link rel="icon" type="image/x-icon" href="">
     <!--begin::Fonts-->
     <link
         rel="stylesheet"
